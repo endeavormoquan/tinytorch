8358c54c160910c083308984705caa4193edb595
endeavormoquan <moquan@mail.nwpu.edu.cn>
2020-09-23 23:23:12 +0800

    c10/core/Backend

---
+++ c10/core/Backend.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#include <c10/core/DeviceType.h>
+#include <c10/core/DispatchKey.h>
+#include <c10/util/Exception.h>
+
+#include <stdexcept>
+
+namespace c10{
+/**
+ * This legacy enum class defines the set of backends supported by old school,
+ * code generated Type-based ATen.  A "backend" in this sense roughly
+ * corresponds to the cartesian product of (device type, layout), but restricted
+ * only to combinations which we actually have kernels for.  Backend does NOT
+ * include dtype.
+ *
+ * The reason we are sunsetting this enum class is because it doesn't allow for
+ * open registration; e.g., if you want to add SparseXLA, you'd have to
+ * edit this enum; you wouldn't be able to do it out of tree.  DispatchKey is
+ * the replacement for Backend which supports open registration.
+ *
+ * NB: The concept of 'Backend' here disagrees with the notion of backend
+ * exposed to users in torch.backends.  Backend here is something like "CPU"
+ * or "SparseCUDA"; backend in torch.backends is something like "MKL" or
+ * "CUDNN".
+ */
+// TODO: NOTE: TLNS(Too Long No See) DispatchKey is the replacement for Backend which supports open registration.
+enum class Backend {
+  CPU,
+  SparseCPU,
+  Undefined,
+  NumOptions
+};
+
+static inline Backend toSparse(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+      return Backend::SparseCPU;
+    case Backend::SparseCPU:
+      return Backend::SparseCPU;
+    default:
+      throw std::runtime_error("Unknown backend");
+  }
+}
+
+static inline Backend toDense(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+      return Backend::CPU;
+    case Backend::SparseCPU:
+      return Backend::CPU;
+    default:
+      throw std::runtime_error("Unknown backend");
+  }
+}
+
+static inline Backend dispatchKeyToBackend(DispatchKey t) {
+  if (t == DispatchKey::CPU) {
+    return Backend::CPU;
+  } else if (t == DispatchKey::SparseCPU) {
+    return Backend::SparseCPU;
+  } else if (t == DispatchKey::Undefined) {
+    return Backend::Undefined;
+  } else {
+    AT_ERROR("Unrecognized tensor type ID: ", t);
+  }
+}
+
+static inline DispatchKey backendToDispatchKey(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+      return DispatchKey::CPU;
+    case Backend::SparseCPU:
+      return DispatchKey::SparseCPU;
+    case Backend::Undefined:
+      return DispatchKey::Undefined;
+    default:
+      throw std::runtime_error("Unknown backend");
+  }
+}
+
+static inline DeviceType backendToDeviceType(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+      return DeviceType::CPU;
+    case Backend::SparseCPU:
+      return DeviceType::CPU;
+    case Backend::Undefined:
+      AT_ERROR("Undefined backend is not a valid device type");
+    default:
+      AT_ERROR("Unknown backend");
+  }
+}
+
+static inline Backend backendToCPU(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+      return Backend::CPU;
+    case Backend::SparseCPU:
+      return Backend::SparseCPU;
+    case Backend::Undefined:
+      return Backend::Undefined;
+    default:
+      AT_ERROR("Unknown backend");
+  }
+}
+
+static inline Backend backendToCUDA(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+    case Backend::SparseCPU:
+    case Backend::Undefined:
+      return Backend::Undefined;
+    default:
+      AT_ERROR("Unknown backend");
+  }
+}
+
+static inline Backend backendToHIP(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+    case Backend::SparseCPU:
+    case Backend::Undefined:
+      return Backend::Undefined;
+    default:
+      AT_ERROR("Unknown backend");
+  }
+}
+
+static inline const char* toString(Backend b) {
+  switch (b) {
+    case Backend::CPU:
+      return "CPU";
+    case Backend::SparseCPU:
+      return "SparseCPU";
+    default:
+      return "UNKNOWN_BACKEND";
+  }
+}
+
+static inline bool isSparse(Backend b) {
+  switch (b) {
+    case Backend::SparseCPU:
+      return true;
+    default:
+      return false;
+  }
+}
+
+}
