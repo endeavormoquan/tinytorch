e2cff9786e887ac04117c5c310f291c1ce6785b9
endeavormoquan <moquan@mail.nwpu.edu.cn>
2020-09-23 23:28:54 +0800

    update Layout, DispatchKey compatibility

--- c10/core/Layout.h
+++ c10/core/Layout.h
@@ -2,6 +2,7 @@
 
 #include <c10/core/Backend.h>
 #include <c10/util/Exception.h>
+#include <c10/core/DispatchKey.h>
 
 #include <iostream>
 
@@ -10,6 +11,16 @@ enum class Layout : int8_t { Strided, Sparse, NumOptions };
 
 constexpr auto kStrided = Layout::Strided;
 constexpr auto kSparse = Layout::Sparse;
+
+// TODO: TODO: Backend seems to be a workaround for compatibility, try to inplace it with DispatchKey directly.
+inline Layout layout_from_dispatchkey(DispatchKey k){
+  switch (k){
+    case DispatchKey::SparseCPU:
+      return Layout::Sparse;
+    default:
+      return Layout::Strided;
+  }
+}
 
 inline Layout layout_from_backend(Backend backend) {
   switch (backend) {
